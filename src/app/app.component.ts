9718b25132d51b523ca1eea2fba42a60e68be03c
Sattar K. Salambayev <salambaev.sattar@gmail.com>
2017-04-29 11:50:02 +0600

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,19 @@
 import { Component } from '@angular/core';
+import {Hero } from './hero';
+
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Batman', superpower: "Money"},
+  { id: 12, name: 'Superman' , superpower: "Strength"},
+  { id: 13, name: 'Flash', superpower:"Speed" },
+  { id: 14, name: 'Aquaman' , superpower: "Water"},
+  { id: 15, name: 'Wonder Woman' , superpower:"Daughter of Gods"},
+  { id: 16, name: 'Green Lantern' , superpower:" Green Ring"},
+  { id: 17, name: 'Supergirl' , superpower: "Strength"},
+  { id: 18, name: 'Zoom' , superpower:"Speed"},
+  { id: 19, name: 'Reverse Flash' , superpower:"Speed"},
+  { id: 20, name: 'Cyborg', superpower:"Mechanic body" }
+];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +21,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Hero details';
+  heroes = HEROES;
+  selectedHero: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
+
+
